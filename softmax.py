aa297763b154efb522af95f512a43d1ddb8149d1
Eleftherios Avramidis <el.avramidis@gmail.com>
2017-07-20 18:46:31 +0100

    Grouped the TensorFlow variables for clarity.

--- softmax.py
+++ softmax.py
@@ -6,10 +6,16 @@ import matplotlib.pyplot as plt
 import numpy
 import random
 
+# Parameters
+learning_rate = 0.05
+training_epochs = 2000
+batch_size = 100
+display_step = 1
+logs_path = 'logs'
+
 # Set the logs folder and delete any files in it
 import glob, os
-logspath = 'logs/*'
-r = glob.glob(logspath)
+r = glob.glob(logs_path + '/*')
 for i in r:
    os.remove(i)
 
@@ -17,44 +23,49 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # Input variable definition and initialisation
-x = tf.placeholder(tf.float32, [None, 784])
-
-# Weights variable definition and initialisation
-W = tf.Variable(tf.zeros([784, 10]))
-
-# Bias variable definition and initialisation
-b = tf.Variable(tf.zeros([10]))
-
-# Model definition
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+x = tf.placeholder(tf.float32, [None, 784], name='InputData')
 
 # Definition of the variable for the target values
-y_ = tf.placeholder(tf.float32, [None, 10])
+y = tf.placeholder(tf.float32, [None, 10], name='LabelData')
 
-# Cost function definition
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+# Weights variable definition and initialisation
+W = tf.Variable(tf.zeros([784, 10]), name='Weights')
 
-# Define the optimisation algorithm
-train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+# Bias variable definition and initialisation
+b = tf.Variable(tf.zeros([10]), name='Bias')
+
+with tf.name_scope('Model'):
+    # Model definition
+    pred = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+
+with tf.name_scope('Loss'):
+    # Cost function definition
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=pred))
+
+with tf.name_scope('Optimiser'):
+    # Define the optimisation algorithm
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+
+with tf.name_scope('Accuracy'):
+    # Model evaluation
+    correct_prediction = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Define and initilise a TensorFlow session
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
-merged = tf.summary.merge_all()
 
-summary_writer = tf.summary.FileWriter('logs', sess.graph)
+summary_writer = tf.summary.FileWriter(logs_path, sess.graph)
 tf.summary.scalar("cross_entropy", cross_entropy)
+
 merged_summary_op = tf.summary.merge_all()
 print("Summary information defined.")
 
 # Train the model
-for i in range(2000):
-  batch_xs, batch_ys = mnist.train.next_batch(100)
-  _, l, summary = sess.run([train_step, cross_entropy, merged_summary_op], feed_dict={x: batch_xs, y_: batch_ys})
+for i in range(training_epochs):
+  batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+  _, l, summary = sess.run([train_step, cross_entropy, merged_summary_op], feed_dict={x: batch_xs, y: batch_ys})
   summary_writer.add_summary(summary, i)
 
-# Model evaluation
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print("Classification accuracy for the test data set: " + str(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})))
+print("Classification accuracy for the test data set: " + str(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})))
