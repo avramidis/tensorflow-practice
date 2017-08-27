ffe4f9f1b88ac2b34d0239442410a8b20eee6b3e
Eleftherios Avramidis <el.avramidis@gmail.com>
2017-08-27 08:20:58 +0100

    Fixed subplot titles for 2nd plot.

--- showdatasample.py
+++ showdatasample.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy
 import random
+from PIL import Image
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -30,12 +31,12 @@ axarr[0][0].imshow(img)
 axarr[0][0].set_title(numpy.where(mnist.train.labels[r+0]==1)[0][0])
 img=rotate(numpy.reshape(mnist.train.images[r+1], (28, 28)), 30, reshape=False)
 axarr[0][1].imshow(img)
-axarr[0][1].set_title(numpy.where(mnist.train.labels[r+0]==1)[0][0])
+axarr[0][1].set_title(numpy.where(mnist.train.labels[r+1]==1)[0][0])
 img=rotate(numpy.reshape(mnist.train.images[r+2], (28, 28)), 30, reshape=False)
 axarr[1][0].imshow(img)
-axarr[1][0].set_title(numpy.where(mnist.train.labels[r+0]==1)[0][0])
+axarr[1][0].set_title(numpy.where(mnist.train.labels[r+2]==1)[0][0])
 img=rotate(numpy.reshape(mnist.train.images[r+3], (28, 28)), 30, reshape=False)
 axarr[1][1].imshow(img)
-axarr[1][1].set_title(numpy.where(mnist.train.labels[r+0]==1)[0][0])
+axarr[1][1].set_title(numpy.where(mnist.train.labels[r+3]==1)[0][0])
 f.subplots_adjust(hspace=0.3)
 plt.show(block=True)
