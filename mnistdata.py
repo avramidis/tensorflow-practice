24ec73fc9f283a80648a7032364681b605e3db88
Eleftherios Avramidis <el.avramidis@gmail.com>
2017-10-29 20:45:02 +0000

    Made changes so the data are from csv files.

--- mnistdata.py
+++ mnistdata.py
@@ -6,14 +6,22 @@
 import numpy
 import random
 import copy
-from tensorflow.examples.tutorials.mnist import input_data
 
-from skimage.filters import threshold_mean
-from skimage.morphology import skeletonize
+from data import Data
 
-from scipy.ndimage import rotate
+#from tensorflow.examples.tutorials.mnist import input_data
 
-class mnistdata(object):
+# from skimage.filters import threshold_mean
+# from skimage.morphology import skeletonize
+
+# from scipy.ndimage import rotate
+
+
+class MnistData(Data):
+    """
+    docstring here
+        :param Data: Class Data.
+    """
 
     mnist = []            # Variable with the MNIST data
 
@@ -26,100 +34,108 @@ class mnistdata(object):
     augmentedimages = []
     augmentedlabels = []
 
-    ## Default constructor.
-	#
-	#  Sets the initial values of the object.
-	#
-	#  @param self                      is the reference to the current object.
+    # Default constructor.
+    #
+    #  Sets the initial values of the object.
+    #
+    #  @param self                      is the reference to the current object.
     def __init__(self):
-        self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+        # mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+        # train_data = mnist.train.images
+        # train_labels = mnist.train.labels
+        # test_data = mnist.test.images
+        # test_labels = mnist.test.labels
 
-        # self.mnist.test.images
-        # self.mnist.test.labels
-        #
-        # self.mnist.train.images
-        # self.mnist.train.labels
+        #super().__init__(train_data, train_labels, test_data, test_labels)
 
-        self.augmentedimages = numpy.empty([len(self.mnist.train.images)*1, 784])
-        self.augmentedlabels = numpy.empty([len(self.mnist.train.images)*1, 10])
+        train_data = numpy.genfromtxt(
+            'train.csv', delimiter=',', skip_header=True)
+        train_labels = numpy.zeros((len(train_data), 10))
+        for i in range(0, len(train_data)):
+            train_labels[i, int(train_data[i, 0])] = 1
+        train_data = train_data[:, 1:]
 
-    def datachange(self):
-        for i in range(len(self.mnist.validation.images)):
-            #image = copy.deepcopy(numpy.reshape(self.mnist.validation.images[i,:], (28, 28)))
-            thresh = threshold_mean(self.mnist.validation.images[i,:])
-            self.mnist.validation.images[i,:] = self.mnist.validation.images[i,:] > thresh
+        # Split train data to test data
+        samples = random.sample(range(len(train_data)), 1000)
+        test_data = train_data[samples, :]
+        test_labels = train_labels[samples, :]
 
-            # # perform skeletonization
-            # image = skeletonize(image)
+        # test_data = numpy.genfromtxt(
+        #     'test.csv', delimiter=',', skip_header=True)
+        # test_labels = numpy.zeros((len(test_data), 10))
+        # for i in range(0, len(test_data)):
+        #     test_labels[i, int(test_data[i, 0])] = 1
+        # test_data = test_data[:, 1:]
 
-            #self.mnist.validation.images[i,:] = numpy.reshape(image, 784)
+        super().__init__(train_data, train_labels, test_data, test_labels)
 
-        for i in range(len(self.mnist.test.images)):
-            #image = copy.deepcopy(numpy.reshape(self.mnist.test.images[i,:], (28, 28)))
-            thresh = threshold_mean(self.mnist.test.images[i,:])
-            self.mnist.test.images[i,:] = self.mnist.test.images[i,:] > thresh
+    # def datachange(self):
+    #     for i in range(len(self.mnist.validation.images)):
+    #         #image = copy.deepcopy(numpy.reshape(self.mnist.validation.images[i,:], (28, 28)))
+    #         thresh = threshold_mean(self.mnist.validation.images[i, :])
+    #         self.mnist.validation.images[i,
+    #                                      :] = self.mnist.validation.images[i, :] > thresh
 
-            # # perform skeletonization
-            # image = skeletonize(image)
+    #         # # perform skeletonization
+    #         # image = skeletonize(image)
 
-            #self.mnist.test.images[i,:] = numpy.reshape(image, 784)
+    #         #self.mnist.validation.images[i,:] = numpy.reshape(image, 784)
 
-    def augmentdata(self):
-        # Do augmentations
-        count = 0
-        for i in range(len(self.mnist.train.images)):
-            #for r in range(4):
-            self.augmentedimages[i,:] = copy.deepcopy(self.mnist.train.images[i,:])
-            self.augmentedlabels[i,:] = copy.deepcopy(self.mnist.train.labels[i,:])
-            #
-            # rotimg=rotate(numpy.reshape(self.mnist.train.images[i,:], (28, 28)), r-2, reshape=False)
-            # self.augmentedimages[count,:] = numpy.reshape(rotimg, 784)
-            #
-            # count = count + 1
+    #     for i in range(len(self.mnist.test.images)):
+    #         #image = copy.deepcopy(numpy.reshape(self.mnist.test.images[i,:], (28, 28)))
+    #         thresh = threshold_mean(self.mnist.test.images[i, :])
+    #         self.mnist.test.images[i,
+    #                                :] = self.mnist.test.images[i, :] > thresh
 
-            #image = copy.deepcopy(numpy.reshape(self.mnist.train.images[i,:], (28, 28)))
+    #         # # perform skeletonization
+    #         # image = skeletonize(image)
 
-            # thresh = threshold_mean(self.augmentedimages[i,:])
-            # self.augmentedimages[i,:] = self.augmentedimages[i,:] > thresh
+    #         #self.mnist.test.images[i,:] = numpy.reshape(image, 784)
 
-            # # # perform skeletonization
-            # # image = skeletonize(image)
-            #
-            #self.augmentedimages[i,:] = numpy.reshape(image, 784)
-            # self.augmentedlabels[i,:] = copy.deepcopy(self.mnist.train.labels[i,:])
+    # def augmentdata(self):
+    #     # Do augmentations
+    #     count = 0
+    #     for i in range(len(self.mnist.train.images)):
+    #         # for r in range(4):
+    #         self.augmentedimages[i, :] = copy.deepcopy(
+    #             self.mnist.train.images[i, :])
+    #         self.augmentedlabels[i, :] = copy.deepcopy(
+    #             self.mnist.train.labels[i, :])
+    #         #
+    #         # rotimg=rotate(numpy.reshape(self.mnist.train.images[i,:], (28, 28)), r-2, reshape=False)
+    #         # self.augmentedimages[count,:] = numpy.reshape(rotimg, 784)
+    #         #
+    #         # count = count + 1
 
-    def __next_batch(self, batch_size):
-        raise NotImplemented
+    #         #image = copy.deepcopy(numpy.reshape(self.mnist.train.images[i,:], (28, 28)))
 
-    def train_next_batch(self, batch_size):
-        # samples = random.sample(range(len(self.mnist.train.images)),  batch_size)
-        # return (self.mnist.train.images[samples,:], self.mnist.train.labels[samples,:])
+    #         # thresh = threshold_mean(self.augmentedimages[i,:])
+    #         # self.augmentedimages[i,:] = self.augmentedimages[i,:] > thresh
 
-        samples = random.sample(range(len(self.augmentedimages)),  batch_size)
-        return (self.augmentedimages[samples,:], self.augmentedlabels[samples,:])
+    #         # # # perform skeletonization
+    #         # # image = skeletonize(image)
+    #         #
+    #         #self.augmentedimages[i,:] = numpy.reshape(image, 784)
+    #         # self.augmentedlabels[i,:] = copy.deepcopy(self.mnist.train.labels[i,:])
 
-        # choice = random.randint(0,1)
-        # if choice==1:
-        #     samples = random.sample(range(len(self.mnist.train.images)),  batch_size)
-        #     return (self.mnist.train.images[samples,:], self.mnist.train.labels[samples,:])
-        # else:
-        #     samples = random.sample(range(len(self.augmentedimages)),  batch_size)
-        #     return (self.augmentedimages[samples,:], self.augmentedlabels[samples,:])
-
-        #return self.__next_batch(batch_size)
 
 if __name__ == "__main__":
 
-    dataset = mnistdata()
-    dataset.augmentdata()
-    dataset.datachange()
+    dataset = MnistData()
 
-    batch = dataset.train_next_batch(4)
+    batch_xs, batch_ys = dataset.train_random_batch(2)
 
-    #batch = dataset.mnist.train.next_batch(4)
+    print(batch_xs)
 
-    print(batch[0])
-    print(type(batch[0]))
+    # dataset.augmentdata()
+    # dataset.datachange()
+
+    # batch = dataset.train_next_batch(4)
+
+    # batch = dataset.mnist.train.next_batch(4)
+
+    # print(batch[0])
+    # print(type(batch[0]))
 
     #
     # batch = dataset.mnist.train.next_batch(2)
--- softmax.py
+++ softmax.py
@@ -1,28 +1,62 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+"""Example Google style docstrings.
 
-import tensorflow as tf
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
+
+import os
 import matplotlib.pyplot as plt
 import numpy
 import random
 import time
+import glob
+import tensorflow as tf
+from mnistdata import MnistData
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 start_time = time.time()
 
 # Parameters
-learning_rate = 0.001
-training_epochs = 6000
+learning_rate = 0.0001
+training_epochs = 10000
 batch_size = 100
 logs_path = 'logs'
 
 # Set the logs folder and delete any files in it
-import glob, os
+
 r = glob.glob(logs_path + '/*')
 for i in r:
-   os.remove(i)
+    os.remove(i)
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = MnistData()
 
 # Input variable definition and initialisation
 x = tf.placeholder(tf.float32, [None, 784], name='InputData')
@@ -38,11 +72,12 @@ b = tf.Variable(tf.zeros([10]), name='Bias')
 
 with tf.name_scope('Model'):
     # Model definition
-    pred = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+    pred = tf.nn.softmax(tf.matmul(x, W) + b)  # Softmax
 
 with tf.name_scope('Loss'):
     # Cost function definition
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=pred))
+    cost = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=pred))
 
 with tf.name_scope('Optimiser'):
     # Define the optimisation algorithm
@@ -51,7 +86,7 @@ with tf.name_scope('Optimiser'):
 
 with tf.name_scope('Accuracy'):
     # Model evaluation
-    correct_prediction = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Initialise the variables
@@ -70,17 +105,21 @@ with tf.Session() as sess:
 
     # Train the model
     for i in range(training_epochs):
-      batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-      _, l, summary = sess.run([optimiser, cost, merged_summary_op], feed_dict={x: batch_xs, y: batch_ys})
+        
+        batch_xs, batch_ys = mnist.train_random_batch(batch_size)
 
-      summary_writer.add_summary(summary, i)
+        _, l, summary = sess.run([optimiser, cost, merged_summary_op],
+                                 feed_dict={x: batch_xs, y: batch_ys})
 
-    finalaccuracy = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
+        summary_writer.add_summary(summary, i)
+
+    finalaccuracy = sess.run(accuracy,
+                             feed_dict={x: mnist.test_data, y: mnist.test_labels})
     print("Classification accuracy for the test data set: " + str(finalaccuracy))
 
 end_time = time.time()
 total_time = end_time - start_time
 print("Runtime: " + str(total_time) + " seconds.")
 
-print("Enter to the command line the following: tensorboard --logdir=" + logs_path)
-print("Using an internet browser navigate to: http://localhost:6006")
+# print("Enter to the command line the following: tensorboard --logdir=" + logs_path)
+# print("Using an internet browser navigate to: http://localhost:6006")
